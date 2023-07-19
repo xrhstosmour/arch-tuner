b6a9c2991ce5f558e9bf543e0a835595ab466e17
Christos <xrhstosmour@gmail.com>
2023-07-19 19:33:18 +0300

    Uppercase variables and terminate coloring to each message

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # Color for the script's messages.
-Cyan='\033[1;36m'
+CYAN='\033[1;36m'
+NO_COLOR='\033[0m'
 
 # Catch exit signal (CTRL + C), to terminate the whole script.
 trap "exit" INT
 
 # Update system.
-echo -e "${Cyan}Updating system..."
+echo -e "${CYAN}Updating system...${NO_COLOR}"
 yes | sudo pacman -Syu
 
 # Install essential packages, if they do not exist.
-echo -e "${Cyan}Installing essential packages..."
+echo -e "${CYAN}Installing essential packages...${NO_COLOR}"
 yes | sudo pacman -S --needed networkmanager base-devel git neovim \
     neofetch btop
 
 # Install paru AUR helper.
-echo -e "${Cyan}Installing paru AUR helper..."
+echo -e "${CYAN}Installing paru AUR helper...${NO_COLOR}"
 if command -v paru &>/dev/null; then
-    echo -e "${Cyan}paru AUR helper, already exists in your system!"
+    echo -e "${CYAN}paru AUR helper, already exists in your system!${NO_COLOR}"
 else
 
     # Delete old paru directory, if it exists.
     if [ -d "paru" ]; then
-        echo -e "${Cyan}Deleting old paru directory..."
+        echo -e "${CYAN}Deleting old paru directory...${NO_COLOR}"
         sudo rm -rf paru
     fi
 
@@ -34,17 +35,17 @@ else
 fi
 
 # Configuring paru AUR helper.
-echo -e "${Cyan}Configuring paru AUR helper..."
-echo -e "${Cyan}Enabling colors in terminal..."
+echo -e "${CYAN}Configuring paru AUR helper...${NO_COLOR}"
+echo -e "${CYAN}Enabling colors in terminal...${NO_COLOR}"
 sed -i '/^#.*Color/s/^#//' /etc/pacman.conf
-echo -e "${Cyan}Skipping review messages..."
+echo -e "${CYAN}Skipping review messages...${NO_COLOR}"
 echo -e "SkipReview" >>/etc/paru.conf
 
 # Installing the display manager.
-echo -e "${Cyan}Installing display manager..."
+echo -e "${CYAN}Installing display manager...${NO_COLOR}"
 yes | paru -S --needed ly-git
 
 # Configuring the display manager.
-echo -e "${Cyan}Configuring display manager..."
+echo -e "${CYAN}Configuring display manager...${NO_COLOR}"
 sudo systemctl enable ly
 sed -i '/^#.*blank_password/s/^#//' /etc/ly/config.ini
