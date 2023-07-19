7c92aedd0fe6ef10370414ef02669c1c3a84b774
Christos <xrhstosmour@gmail.com>
2023-07-19 20:13:05 +0300

    Add new line between messages

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -8,23 +8,23 @@ NO_COLOR='\033[0m'
 trap "exit" INT
 
 # Update system.
-echo -e "${CYAN}Updating system...${NO_COLOR}"
+echo -e "\n${CYAN}Updating system...${NO_COLOR}"
 yes | sudo pacman -Syu
 
 # Install essential packages, if they do not exist.
-echo -e "${CYAN}Installing essential packages...${NO_COLOR}"
+echo -e "\n${CYAN}Installing essential packages...${NO_COLOR}"
 yes | sudo pacman -S --needed networkmanager base-devel git neovim \
     neofetch btop
 
 # Install paru AUR helper.
-echo -e "${CYAN}Installing paru AUR helper...${NO_COLOR}"
+echo -e "\n${CYAN}Installing paru AUR helper...${NO_COLOR}"
 if command -v paru &>/dev/null; then
-    echo -e "${CYAN}paru AUR helper, already exists in your system!${NO_COLOR}"
+    echo -e "\n${CYAN}paru AUR helper, already exists in your system!${NO_COLOR}"
 else
 
     # Delete old paru directory, if it exists.
     if [ -d "paru" ]; then
-        echo -e "${CYAN}Deleting old paru directory...${NO_COLOR}"
+        echo -e "\n${CYAN}Deleting old paru directory...${NO_COLOR}"
         sudo rm -rf paru
     fi
 
@@ -42,10 +42,10 @@ echo -e "${CYAN}Skipping review messages...${NO_COLOR}"
 echo -e "SkipReview" >>/etc/paru.conf
 
 # Installing the display manager.
-echo -e "${CYAN}Installing display manager...${NO_COLOR}"
+echo -e "\n${CYAN}Installing display manager...${NO_COLOR}"
 yes | paru -S --needed ly-git
 
 # Configuring the display manager.
-echo -e "${CYAN}Configuring display manager...${NO_COLOR}"
+echo -e "\n${CYAN}Configuring display manager...${NO_COLOR}"
 sudo systemctl enable ly
 sed -i '/^#.*blank_password/s/^#//' /etc/ly/config.ini
