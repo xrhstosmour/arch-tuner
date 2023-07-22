72a386c062cb01978ee361427ef0cd8ba80fd046
Christos <xrhstosmour@gmail.com>
2023-07-22 20:12:06 +0300

    Install poetry plugins system wide

--- scripts/development.sh
+++ scripts/development.sh
@@ -24,7 +24,7 @@ echo -e "\n${CYAN}Installing programming languages...${NO_COLOR}"
 
 # Installing Python.
 echo -e "\n${CYAN}Installing Python...${NO_COLOR}"
-paru -S --noconfirm --needed python python-pip python-poetry
+paru -S --noconfirm --needed python python-pip python-poetry python-poetry-plugin-up
 
 # Configuring Python.
 echo -e "\n${CYAN}Configuring Python...${NO_COLOR}"
@@ -32,12 +32,3 @@ echo -e "\n${CYAN}Configuring Python...${NO_COLOR}"
 # Configuring Poetry.
 echo -e "\n${CYAN}Configuring Poetry...${NO_COLOR}"
 poetry config virtualenvs.in-project true
-
-# Setting environment variable both for fish and bash shell.
-# This is needed to avoid error while adding poetry plugins.
-set -x PYTHON_KEYRING_BACKEND 'keyring.backends.null.Keyring'
-echo 'export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring' >>~/.bashrc 2>/dev/null
-echo 'export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring' >>~/.profile 2>/dev/null
-
-# Installing poetry plugins in a new shell instance to get the environment variable.
-"$SHELL" -c 'poetry self add poetry-plugin-up'
