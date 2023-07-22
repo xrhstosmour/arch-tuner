4ad4e17e6fbe9ce30281df1520dad572ca68a91f
Christos <xrhstosmour@gmail.com>
2023-07-22 19:15:23 +0300

    Fix script to be able to run it through fish shell

--- scripts/development.sh
+++ scripts/development.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/fish
 
 # Color for the script's messages.
 CYAN='\033[1;36m'
@@ -11,7 +11,7 @@ trap "exit" INT
 set -e
 
 # Installing development tools.
-echo -e "\n${CYAN}Installing development tools...${NO_COLOR}"
+echo "\n${CYAN}Installing development tools...${NO_COLOR}"
 paru -S --noconfirm --needed vscodium-bin remmina-git gitkraken obsidian \
     postman-bin etcher-bin docker docker-compose
 
@@ -20,16 +20,17 @@ echo "Configuring development tools..."
 sudo systemctl enable docker && sudo systemctl start docker
 
 # Installing programming languages.
-echo -e "\n${CYAN}Installing programming languages...${NO_COLOR}"
+echo "\n${CYAN}Installing programming languages...${NO_COLOR}"
 
 # Installing Python.
-echo -e "\n${CYAN}Installing Python...${NO_COLOR}"
+echo "\n${CYAN}Installing Python...${NO_COLOR}"
 paru -S --noconfirm --needed python python-pip python-poetry
 
 # Configuring Python.
-echo -e "\n${CYAN}Configuring Python...${NO_COLOR}"
+echo "\n${CYAN}Configuring Python...${NO_COLOR}"
 
 # Configuring Poetry.
-echo -e "\n${CYAN}Configuring Poetry...${NO_COLOR}"
+echo "\n${CYAN}Configuring Poetry...${NO_COLOR}"
 set -x PYTHON_KEYRING_BACKEND 'keyring.backends.null.Keyring'
-poetry self add poetry-plugin-up
+poetry config virtualenvs.in-project true
+poetry plugin add poetry-plugin-up
