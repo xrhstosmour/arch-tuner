38a33d94bab4c3f5e073529b2246084268988ef3
Christos <xrhstosmour@gmail.com>
2023-07-22 19:46:36 +0300

    Adding colors and more messages

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 
 # Color for the script's messages.
 CYAN='\033[1;36m'
+BOLD_GREEN='\e[1;${GREEN}m'
 NO_COLOR='\033[0m'
 
 # Catch exit signal (CTRL + C), to terminate the whole script.
@@ -18,16 +19,21 @@ chmod +x ./essentials.sh
 chmod +x ./development.sh
 
 # Start by executing the essentials script.
+echo -e "\n${CYAN}Running essentials script...${NO_COLOR}"
 ./essentials.sh
+echo -e "\n${CYAN}Essentials script finished!${NO_COLOR}"
 
-#  Proceed with the development tools and programming languages.
-read -p "\n${CYAN}Do you want to install development tools and programming languages? \nY/N:${NO_COLOR}" answer
+# Proceed with the development script.
+echo "\n"
+read -p "${BOLD_GREEN}Do you want to install development tools and programming languages? Y/N: ${NO_COLOR}" answer
 
 # Convert the answer to lowercase to accept 'Y', 'y', 'N', 'n' as valid.
 answer=${answer,,}
 
 if [[ "$answer" == "y" ]]; then
+    echo -e "\n${CYAN}Running development script...${NO_COLOR}"
     ./development.sh
+    echo -e "\n${CYAN}Development script finished!${NO_COLOR}"
 elif [[ "$answer" != "n" ]]; then
     echo "Invalid input!"
 fi
