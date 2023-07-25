e1520ad2a6bb1604f811d83a847ad719d002fb30
Christos <xrhstosmour@gmail.com>
2023-07-25 17:17:00 +0300

    Add prompt and shell configuration files

---
+++ scripts/configurations/shell/aliases.fish
@@ -0,0 +1,11 @@
+# Custom aliases for terminal tools.
+abbr cat "bat"
+abbr ls "exa --git --icons --long --header --color=always --group-directories-first"
+abbr cp "xcp"
+abbr rm "rip"
+abbr eva "calc"
+abbr cd "z"
+abbr find "fd"
+abbr sed "sd"
+abbr up "topgrade"
+abbr mkdir "mkdir -p"
---
+++ scripts/configurations/shell/configuration.fish
@@ -0,0 +1,5 @@
+# Enabling starship at fish prompt.
+starship init fish | source
+
+# Enabling zoxide at fish prompt.
+zoxide init fish | source
