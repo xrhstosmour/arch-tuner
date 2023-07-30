6a3bcdd9ecc61ad9823dfc89788f29ca7df7e174
Christos <xrhstosmour@gmail.com>
2023-07-30 14:47:56 +0300

    Enable trackability reduction, keystroke anonymization and microcode updates

--- scripts/security.sh
+++ scripts/security.sh
@@ -64,6 +64,45 @@ sudo systemctl enable clamav-freshclam.service
 sudo systemctl start clamav-daemon.service
 sudo systemctl enable clamav-daemon.service
 
-# Enable real-time scanning.
+# Enabling real-time scanning.
 echo -e "\n${BOLD_CYAN}Enabling real-time scanning...${NO_COLOR}"
 sudo clamonacc --move=/qrntn
+
+# Check if NetworkManager is installed and running.
+if command -v NetworkManager >/dev/null && systemctl is-active --quiet NetworkManager; then
+
+    # Enabling trackability reduction.
+    echo -e "\n${BOLD_CYAN}Enabling trackability reduction...${NO_COLOR}"
+
+    # Create or overwrite the configuration file with the desired settings
+    echo -e "[device]\nwifi.scan-rand-mac-address=yes\n\n[connection]\nwifi.cloned-mac-address=random\nethernet.cloned-mac-address=random" >"/etc/NetworkManager/conf.d/00-macrandomize.conf"
+
+    # Restart the NetworkManager service to apply the changes
+    systemctl restart NetworkManager
+fi
+
+# Installing keystroke anonymization.
+echo -e "\n${BOLD_CYAN}Installing keystroke anonymization...${NO_COLOR}"
+paru -S --noconfirm --needed kloak-git
+
+# Get the CPU manufacturer.
+cpu_manufacturer=$(grep -m 1 -oP 'vendor_id\s*:\s*\K.*' /proc/cpuinfo)
+
+# Initialize a flag indicating if a microcode update was installed.
+update_installed=false
+
+# Install the appropriate microcode based on the CPU manufacturer.
+if [[ $cpu_manufacturer == *'GenuineIntel'* ]]; then
+    echo -e "\n${BOLD_CYAN}Installing Intel microcode updates...${NO_COLOR}"
+    sudo pacman -S --noconfirm --needed intel-ucode
+    update_installed=true
+elif [[ $cpu_manufacturer == *'AuthenticAMD'* ]]; then
+    echo -e "\n${BOLD_CYAN}Installing AMD microcode updates...${NO_COLOR}"
+    sudo pacman -S --noconfirm --needed amd-ucode
+    update_installed=true
+fi
+
+# Update grub to apply microcode updates at boot, only if an update was installed.
+if $update_installed; then
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+fi
