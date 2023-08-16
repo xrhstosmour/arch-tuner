22642192bb16d15ddef0bcf2aada7e9af4ae74b8
Christos <xrhstosmour@gmail.com>
2023-08-16 17:12:19 +0300

    Minor changes and example usage at append_line_to_file function

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -4,12 +4,13 @@
 source ./constants.sh
 
 # Function to check if a line exists in a file and add it if it does not.
+# append_line_to_file "/file/path/with.extension" "Line To Append" "Message to print if the line is appended."
 append_line_to_file() {
     file_path="$1"
-    line_to_check="$2"
+    line_to_append="$2"
     message="$3"
 
-    if ! grep -qxF "$line_to_check" "$file_path"; then
+    if ! grep -qxF "$line_to_append" "$file_path"; then
 
         # Print message if it exists.
         if [ -n "$message" ]; then
@@ -17,7 +18,7 @@ append_line_to_file() {
         fi
 
         # Append line to file.
-        echo "$line_to_check" | sudo tee -a "$file_path" >/dev/null
+        echo "$line_to_append" | sudo tee -a "$file_path" >/dev/null
 
         # Return true to indicate that a change was made.
         return true
