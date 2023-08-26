ba9ef283c516f44105722320d1402f1958411897
Christos <xrhstosmour@gmail.com>
2023-08-26 19:03:02 +0300

    Refactor desktop script to use the new function for installing packages

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Color for the script's messages.
-BOLD_CYAN='\e[1;36m'
-NO_COLOR='\e[0m'
-
 # Catch exit signal (CTRL + C), to terminate the whole script.
 trap "exit" INT
 
 # Terminate script on error.
 set -e
 
+# Import constant variables and functions.
+source ./constants.sh
+source ./functions.sh
+
 # Installing desktop applications.
-echo -e "\n${BOLD_CYAN}Installing desktop applications...${NO_COLOR}"
-xargs -a ./packages/desktop.txt -r -- paru -S --noconfirm --needed
+install_packages_from_file "./packages/desktop.txt"
