a8e39e3de5e56885155c70371a15bc31be07a15a
Christos <xrhstosmour@gmail.com>
2023-08-31 17:15:52 +0300

    Script to install fonts

---
+++ scripts/helpers/essentials/fonts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Catch exit signal (CTRL + C), to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Import functions and constant variables.
+source ../functions.sh
+source ../../core/constants.sh
+
+# Constant variable for the fonts to install.
+FONTS="ttf-firacode-nerd"
+
+# Check if at least one font is not installed.
+if are_packages_installed "$FONTS" "$AUR_PACKAGE_MANAGER"; then
+    log_info "Installing fonts..."
+
+    # Installing fonts.
+    install_packages "$FONTS" "$AUR_PACKAGE_MANAGER"
+fi
