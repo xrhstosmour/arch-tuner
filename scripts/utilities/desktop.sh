b05edd8bfb42b02c6f55196bdc04cba4d3fdd0d4
Christos <xrhstosmour@gmail.com>
2023-09-01 15:46:39 +0300

    Script to install and configure desktop packages

---
+++ scripts/utilities/desktop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Catch exit signal (CTRL + C), to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Constant variable of the scripts' working directory to use for relative paths.
+SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions and constant variables.
+source "$SCRIPT_DIRECTORY/../helpers/functions.sh"
+source "$SCRIPT_DIRECTORY/../core/constants.sh"
+
+# Constant variable for the file path containing the desktop applications to install.
+DESKTOP_PACKAGES="$SCRIPT_DIRECTORY/../packages/desktop.txt"
+
+# Check if at least one desktop package is not installed.
+if ! are_packages_installed "$DESKTOP_PACKAGES" "$AUR_PACKAGE_MANAGER"; then
+    log_info "Installing desktop applications..."
+
+    # Installing desktop packages.
+    install_packages "$DESKTOP_PACKAGES" "$AUR_PACKAGE_MANAGER"
+fi
