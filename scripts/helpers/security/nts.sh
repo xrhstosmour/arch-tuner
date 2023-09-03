e75dc70ad0ebb5b48e5f521198b563439b61bf48
Christos <xrhstosmour@gmail.com>
2023-09-03 17:34:41 +0300

    Script to install and configure encrypted network time security

---
+++ scripts/helpers/security/nts.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Catch exit signal (CTRL + C), to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Constant variable of the scripts' working directory to use for relative paths.
+NTS_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions.
+source "$NTS_SCRIPT_DIRECTORY/../functions.sh"
+
+# Initialize a flag indicating if an encrypted network time security change has been made.
+nts_changes_made=1
+
+# Encrypted network time security constant configuration variables.
+NTS_CONFIGURATION_DIRECTORY="/etc/chrony/"
+NTS_SYSTEM_CONFIGURATION_DIRECTORY="/etc/sysconfig"
+NTS_CONFIGURATION="/etc/chrony/chrony.conf"
+NTS_CONFIGURATION_TO_PASS="$NTS_SCRIPT_DIRECTORY/../../configurations/network/time.conf"
+NTS_SYSTEM_CONFIGURATION="/etc/sysconfig/chronyd"
+
+# Install encrypted network time security.
+install_packages "chrony" "$AUR_PACKAGE_MANAGER" "Installing encrypted network time security..."
+
+# Copy the configuration file only if it is not the same as the current one.
+if ! compare_files "$NTS_CONFIGURATION" "$NTS_CONFIGURATION_TO_PASS"; then
+    log_info "Configuring encrypted network time security..."
+    sudo mkdir -p "$NTS_CONFIGURATION_DIRECTORY"
+    sudo cp -f "$NTS_CONFIGURATION_TO_PASS" "$NTS_CONFIGURATION"
+    sudo chmod 644 "$NTS_CONFIGURATION"
+    sudo chown root:root "$NTS_CONFIGURATION"
+
+    # Set the nts_changes_made flag to 0 (true).
+    nts_changes_made=0
+fi
+
+# Add the seccomp filter option to the environment file only if not exists.
+if ! grep -q 'OPTIONS="-F 1"' "$NTS_SYSTEM_CONFIGURATION" 2>/dev/null; then
+    log_info "Limiting access to encrypted network time security application..."
+    sudo mkdir -p "$NTS_SYSTEM_CONFIGURATION_DIRECTORY"
+    echo 'OPTIONS="-F 1"' | sudo tee "$NTS_SYSTEM_CONFIGURATION" >/dev/null
+
+    # Set the nts_changes_made flag to 0 (true).
+    nts_changes_made=0
+fi
+
+# Restart the network time security to apply the changes.
+if [ $nts_changes_made -eq 0 ]; then
+    stop_service "chronyd" "Stopping encrypted NTS service..."
+    start_service "chronyd" "Starting encrypted NTS service..."
+    enable_service "chronyd" "Enabling encrypted NTS service..."
+fi
