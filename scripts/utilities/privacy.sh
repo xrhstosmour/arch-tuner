f4da4befac3fd2457c3268336919fde7b87608b2
Christos <xrhstosmour@gmail.com>
2023-09-05 19:14:45 +0300

    Script to install and configure privacy functionalities and packages

---
+++ scripts/utilities/privacy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Catch exit signal (CTRL + C), to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Constant variable of the scripts' working directory to use for relative paths.
+PRIVACY_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions.
+source "$PRIVACY_SCRIPT_DIRECTORY/../helpers/functions.sh"
+
+# Array of privacy scripts.
+privacy_scripts=(
+    $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/network.sh
+    $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/keyboard.sh
+    $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/umask.sh
+)
+
+# Give execution permission to all needed scripts.
+give_execution_permission_to_scripts "${privacy_scripts[@]}" "Giving execution permission to all privacy scripts..."
+
+# Configure network.
+sh $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/network.sh
+
+# Install and configure keystroke anonymization.
+sh $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/keyboard.sh
+
+# Configure umask.
+sh $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/umask.sh
+
+# TODO: Implement encrypted swap.
