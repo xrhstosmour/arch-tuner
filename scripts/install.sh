2112dfa06f3df1bd6f0136db2d3972b68043fdd2
Christos <xrhstosmour@gmail.com>
2023-09-06 20:22:38 +0300

    Script to install and configure dotfile packages and functionalities

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,37 +6,31 @@ trap "exit" INT
 # Terminate script on error.
 set -e
 
-# Import constant variables.
-source ./constants.sh
+# Constant variable of the scripts' working directory to use for relative paths.
+INSTALL_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions.
+source "$INSTALL_SCRIPT_DIRECTORY/helpers/functions/logs.sh"
+
+# ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 
 # Wait for user approval.
-echo -e "\n${BOLD_RED}BACKUP EVERYTHING BEFORE PROCEEDING!${NO_COLOR}"
-echo -e "\n${BOLD_YELLOW}If not, exit script and re-run after backup!${NO_COLOR}"
-echo -e "\n${BOLD_CYAN}Press ENTER to continue within next 10 seconds!${NO_COLOR}"
+log_error "BACKUP EVERYTHING BEFORE PROCEEDING!"
+log_warning "If not, exit script and re-run after backup!"
+log_info "Press ENTER to continue within next 10 seconds!"
 
-# Read user input with a 10 second timeout
+# Read user input with a 10 second timeout.
 if read -t 10; then
-    echo -e "\n${BOLD_CYAN}Starting installing procedure...${NO_COLOR}"
+    log_info "Starting installing procedure..."
 else
-    echo -e "\n${BOLD_CYAN}Terminating script...${NO_COLOR}"
+    log_info "Terminating script..."
     exit 1
 fi
 
-# Give execution permission to all scripts.
-echo -e "\n${BOLD_CYAN}Giving execution permission to all scripts...${NO_COLOR}"
-chmod +x ./functions.sh
-chmod +x ./constants.sh
-chmod +x ./essentials.sh
-chmod +x ./security.sh
-chmod +x ./privacy.sh
-chmod +x ./interface.sh
-chmod +x ./desktop.sh
-chmod +x ./development.sh
-
 # Start by executing the essentials script.
-echo -e "\n${BOLD_CYAN}Executing essentials script...${NO_COLOR}"
-./essentials.sh
-echo -e "\n${BOLD_CYAN}Essentials script execution finished!${NO_COLOR}"
+log_info "Executing essentials script..."
+sh $INSTALL_SCRIPT_DIRECTORY/utilities/essentials.sh
+log_info "Essentials script execution finished!"
 
 # TODO: Convert the repeatable code into a function.
 # Default interface answer.
@@ -44,18 +38,18 @@ interface_answer=""
 
 # Proceed with the interface script.
 while [[ "$interface_answer" != "y" && "$interface_answer" != "n" ]]; do
-    echo -e "\n${BOLD_GREEN}Do you want to install display manager and GPU drivers? Y/N: ${NO_COLOR}"
+    log_info "Do you want to install display manager and GPU drivers? Y/N: "
     read -r interface_answer
 
     # Convert the answer to lowercase to accept 'Y', 'y', 'N', 'n' as valid.
     interface_answer=${interface_answer,,}
 
     if [[ "$interface_answer" == "y" ]]; then
-        echo -e "\n${BOLD_CYAN}Executing interface script...${NO_COLOR}"
-        ./interface.sh
-        echo -e "\n${BOLD_CYAN}Interface script execution finished!${NO_COLOR}"
+        log_info "Executing interface script..."
+        sh $INSTALL_SCRIPT_DIRECTORY/utilities/interface.sh
+        log_info "Interface script execution finished!"
     elif [[ "$interface_answer" != "n" ]]; then
-        echo "Invalid input!"
+        log_error "Invalid input!"
     fi
 done
 
@@ -64,18 +58,18 @@ desktop_answer=""
 
 # Proceed with the desktop script.
 while [[ "$desktop_answer" != "y" && "$desktop_answer" != "n" ]]; do
-    echo -e "\n${BOLD_GREEN}Do you want to install desktop applications? Y/N: ${NO_COLOR}"
+    log_info "Do you want to install desktop applications? Y/N: "
     read -r desktop_answer
 
     # Convert the answer to lowercase to accept 'Y', 'y', 'N', 'n' as valid.
     desktop_answer=${desktop_answer,,}
 
     if [[ "$desktop_answer" == "y" ]]; then
-        echo -e "\n${BOLD_CYAN}Executing desktop script...${NO_COLOR}"
-        ./desktop.sh
-        echo -e "\n${BOLD_CYAN}desktop script execution finished!${NO_COLOR}"
+        log_info "Executing desktop script..."
+        sh $INSTALL_SCRIPT_DIRECTORY/utilities/desktop.sh
+        log_info "Desktop script execution finished!"
     elif [[ "$desktop_answer" != "n" ]]; then
-        echo "Invalid input!"
+        log_error "Invalid input!"
     fi
 done
 
@@ -84,27 +78,27 @@ development_answer=""
 
 # Proceed with the development script.
 while [[ "$development_answer" != "y" && "$development_answer" != "n" ]]; do
-    echo -e "\n${BOLD_GREEN}Do you want to install development tools and programming languages? Y/N: ${NO_COLOR}"
+    log_info "Do you want to install development tools and programming languages? Y/N: "
     read -r development_answer
 
     # Convert the answer to lowercase to accept 'Y', 'y', 'N', 'n' as valid.
     development_answer=${development_answer,,}
 
     if [[ "$development_answer" == "y" ]]; then
-        echo -e "\n${BOLD_CYAN}Executing development script...${NO_COLOR}"
-        ./development.sh
-        echo -e "\n${BOLD_CYAN}Development script execution finished!${NO_COLOR}"
+        log_info "Executing development script..."
+        sh $INSTALL_SCRIPT_DIRECTORY/utilities/development.sh
+        log_info "Development script execution finished!"
     elif [[ "$development_answer" != "n" ]]; then
-        echo "Invalid input!"
+        log_error "Invalid input!"
     fi
 done
 
 # Run the privacy script.
-echo -e "\n${BOLD_CYAN}Executing privacy script...${NO_COLOR}"
-./privacy.sh
-echo -e "\n${BOLD_CYAN}Privacy script execution finished!${NO_COLOR}"
+log_info "Executing privacy script..."
+sh $INSTALL_SCRIPT_DIRECTORY/utilities/privacy.sh
+log_info "Privacy script execution finished!"
 
 # Run the security script at the end.
-echo -e "\n${BOLD_CYAN}Executing security script...${NO_COLOR}"
-./security.sh
-echo -e "\n${BOLD_CYAN}Security script execution finished!${NO_COLOR}"
+log_info "Executing security script..."
+sh $INSTALL_SCRIPT_DIRECTORY/utilities/security.sh
+log_info "Security script execution finished!"
