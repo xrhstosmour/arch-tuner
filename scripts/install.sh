6ae0f439472ca64f29f5ca87cb635f1b0ee676ae
Christos <xrhstosmour@gmail.com>
2023-09-10 17:18:39 +0300

    Add missing reboot function imports

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,7 @@ INSTALL_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Import functions.
 source "$INSTALL_SCRIPT_DIRECTORY/helpers/functions/ui.sh"
+source "$INSTALL_SCRIPT_DIRECTORY/helpers/functions/system.sh"
 
 # ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 # ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
