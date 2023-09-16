bad039a4ddb802beb3efd70848305e70dd10014e
Christos <xrhstosmour@gmail.com>
2023-09-16 21:22:30 +0300

    Trying to make keystroke anonymization to work

--- scripts/helpers/privacy/keyboard.sh
+++ scripts/helpers/privacy/keyboard.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# TODO: There are open issues with the kloak package, so this script is not working properly, and it is not recommended to use for now.
+# ? When enabling the service, every password input is wrong.
+# ? https://github.com/vmonaco/kloak/issues/12
+
 # Catch exit signal (CTRL + C), to terminate the whole script.
 trap "exit" INT
 
@@ -33,7 +37,7 @@ if [ ! -f "$KEYSTROKE_ANONYMIZATION_CONFIGURATION" ]; then
         ExecStart=/usr/bin/$KEYSTROKE_ANONYMIZATION_PACKAGE
 
         [Install]
-        WantedBy=default.target" | sudo tee "$KEYSTROKE_ANONYMIZATION_CONFIGURATION" >/dev/null
+        WantedBy=multi-user.target" | sudo tee "$KEYSTROKE_ANONYMIZATION_CONFIGURATION" >/dev/null
 fi
 
 # Add the user to the input group.
@@ -42,6 +46,12 @@ if ! groups $USER | grep -q '\binput\b'; then
     sudo usermod -aG input $USER
 fi
 
+# Set the keyboard repeat rate to 300ms and delay to 25ms.
+if ! xset q | grep -q "rate:  300    delay:  25"; then
+    log_info "Setting keyboard repeat rate to 300ms and delay to 25ms..."
+    xset r rate 300 25
+fi
+
 # Start and enable the service.
-start_service "$KEYSTROKE_ANONYMIZATION_PACKAGE" "Starting $KEYSTROKE_ANONYMIZATION_PACKAGE service..." "user"
-enable_service "$KEYSTROKE_ANONYMIZATION_PACKAGE" "Enabling $KEYSTROKE_ANONYMIZATION_PACKAGE service..." "user"
+start_service "$KEYSTROKE_ANONYMIZATION_PACKAGE" "Starting $KEYSTROKE_ANONYMIZATION_PACKAGE service..."
+enable_service "$KEYSTROKE_ANONYMIZATION_PACKAGE" "Enabling $KEYSTROKE_ANONYMIZATION_PACKAGE service..."
