bcfa54f37c32d269d669e69c629580749b2858e6
Christos <xrhstosmour@gmail.com>
2023-09-16 15:03:44 +0300

    Function to change a flag value stored in a script

--- scripts/helpers/functions/strings.sh
+++ scripts/helpers/functions/strings.sh
@@ -8,3 +8,12 @@ STRINGS_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 trim_string() {
     echo "$1" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
 }
+
+# Function to change a flag value to the given value.
+# change_flag_value "flag" "value" "script_path"
+change_flag_value() {
+    local flag=$1
+    local value=$2
+    local script_path=$3
+    sed -i "s/^$flag=.*\$/$flag=$value/" "$script_path"
+}
