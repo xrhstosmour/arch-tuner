567e4af5aaba9ddaa727971bace821eefac96e77
Christos <xrhstosmour@gmail.com>
2023-09-18 16:26:03 +0300

    Installing privacy packages

--- scripts/utilities/privacy.sh
+++ scripts/utilities/privacy.sh
@@ -15,6 +15,18 @@ source "$PRIVACY_SCRIPT_DIRECTORY/../helpers/functions/filesystem.sh"
 # ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 # ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
 
+# Constant variable for the file path containing the privacy applications to install.
+PRIVACY_PACKAGES="$PRIVACY_SCRIPT_DIRECTORY/../packages/privacy.txt"
+
+# Check if at least one privacy package is not installed.
+are_privacy_packages_installed=$(are_packages_installed "$PRIVACY_PACKAGES" "$AUR_PACKAGE_MANAGER")
+if [ "$are_privacy_packages_installed" = "false" ]; then
+    log_info "Installing privacy applications..."
+
+    # Install privacy packages.
+    install_packages "$PRIVACY_PACKAGES" "$AUR_PACKAGE_MANAGER"
+fi
+
 # Configure network.
 sh $PRIVACY_SCRIPT_DIRECTORY/../helpers/privacy/network.sh
 
