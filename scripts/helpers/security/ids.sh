0a6e2fb38d05f3cdf5bc3649de5957691fe532ef
Christos <xrhstosmour@gmail.com>
2023-10-04 00:53:21 +0300

    Exclude /proc and /sys directory from your search

--- scripts/helpers/security/ids.sh
+++ scripts/helpers/security/ids.sh
@@ -25,9 +25,8 @@ BOOT_DIRECTORY="/boot"
 SUID_PERMISSION="4000"
 SGID_PERMISSION="2000"
 
-# TODO: Check if this is working or not.
 # Find command to locate all files with SUID and SGID enabled outside the specified directories.
-FIND_SUID_AND_SGID_COMMAND="sudo find / \( -path \"$SBIN_DIRECTORY\" -o -path \"$USR_DIRECTORY\" -o -path \"$BIN_DIRECTORY\" -o -path \"$OPT_DIRECTORY\" -o -path \"$ROOT_DIRECTORY\" -o -path \"$BOOT_DIRECTORY\" \) -prune -o \( -perm /$SUID_PERMISSION -o -perm /$SGID_PERMISSION \) -type f -print"
+FIND_SUID_AND_SGID_COMMAND="sudo find / -path /proc -prune -o -path /sys -prune -o \( -path \"$SBIN_DIRECTORY\" -o -path \"$USR_DIRECTORY\" -o -path \"$BIN_DIRECTORY\" -o -path \"$OPT_DIRECTORY\" -o -path \"$ROOT_DIRECTORY\" -o -path \"$BOOT_DIRECTORY\" \) -prune -o \( -perm /$SUID_PERMISSION -o -perm /$SGID_PERMISSION \) -type f -print"
 
 # Check if any file has SUID enabled outside the specified directories.
 if eval "$FIND_SUID_AND_SGID_COMMAND" | grep -q .; then
