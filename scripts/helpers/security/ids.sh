126f70a1532029b4d22aaf231623666d6c6d3bd6
Christos <xrhstosmour@gmail.com>
2023-10-25 14:17:11 +0300

    Check without prune which stops execution at first excluding path

--- scripts/helpers/security/ids.sh
+++ scripts/helpers/security/ids.sh
@@ -49,7 +49,7 @@ for path in "${EXCLUDE_PATHS[@]}"; do
 done
 
 # Find all binaries with setuid or setgid bit set, excluding specified paths.
-suid_sgid_binary_files=$(sudo find / $EXCLUDE_ARGUMENTS -type f \( -perm -4000 -o -perm -2000 \) -print 2>/dev/null)
+suid_sgid_binary_files=$(sudo find / \( \( $EXCLUDE_ARGUMENTS \) -false -o -type f \( -perm -4000 -o -perm -2000 \) \) -print 2>/dev/null)
 if [[ -z "$suid_sgid_binary_files" ]]; then
     log_info "No SUID/SGID binaries found!"
     exit 0
