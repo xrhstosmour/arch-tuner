4d498d5e16a746ca127eb1fdcc2646d5378139ef
Christos <xrhstosmour@gmail.com>
2023-11-04 13:07:44 +0200

    Install themes, icons, cursors and fonts

---
+++ scripts/helpers/essentials/theming.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Catch exit signal (CTRL + C), to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Constant variable of the scripts' working directory to use for relative paths.
+THEMING_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions.
+source "$THEMING_SCRIPT_DIRECTORY/../functions/packages.sh"
+
+# ? Importing constants.sh is not needed, because it is already sourced in the logs script.
+# ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
+
+# Constant variables for the file paths containing the themes, icons, cursors and fonts to install.
+THEMES="$THEMING_SCRIPT_DIRECTORY/../../packages/essentials/themes.txt"
+ICONS="$THEMING_SCRIPT_DIRECTORY/../../packages/essentials/icons.txt"
+CURSORS="$THEMING_SCRIPT_DIRECTORY/../../packages/essentials/cursors.txt"
+FONTS="$THEMING_SCRIPT_DIRECTORY/../../packages/essentials/fonts.txt"
+
+# Check if at least one theme is not installed.
+are_theme_packages_installed=$(are_packages_installed "$THEMES" "$AUR_PACKAGE_MANAGER")
+if [ "$are_theme_packages_installed" = "false" ]; then
+    log_info "Installing themes..."
+
+    # Install themes.
+    install_packages "$THEMES" "$AUR_PACKAGE_MANAGER"
+fi
+
+# Check if at least one icon is not installed.
+are_icon_packages_installed=$(are_packages_installed "$ICONS" "$AUR_PACKAGE_MANAGER")
+if [ "$are_icon_packages_installed" = "false" ]; then
+    log_info "Installing icons..."
+
+    # Install icons.
+    install_packages "$ICONS" "$AUR_PACKAGE_MANAGER"
+fi
+
+# Check if at least one cursor is not installed.
+are_cursor_packages_installed=$(are_packages_installed "$CURSORS" "$AUR_PACKAGE_MANAGER")
+if [ "$are_cursor_packages_installed" = "false" ]; then
+    log_info "Installing cursors..."
+
+    # Install cursors.
+    install_packages "$CURSORS" "$AUR_PACKAGE_MANAGER"
+fi
+
+# Check if at least one font is not installed.
+are_font_packages_installed=$(are_packages_installed "$FONTS" "$AUR_PACKAGE_MANAGER")
+if [ "$are_font_packages_installed" = "false" ]; then
+    log_info "Installing fonts..."
+
+    # Install fonts.
+    install_packages "$FONTS" "$AUR_PACKAGE_MANAGER"
+fi
