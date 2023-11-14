41a938330a9d986b17cfc277e5305b130d64f02d
Christos <xrhstosmour@gmail.com>
2023-11-14 22:19:55 +0200

    Disable previous greeters if exist

--- scripts/helpers/interface/greeter.sh
+++ scripts/helpers/interface/greeter.sh
@@ -16,14 +16,19 @@ source "$GREETER_SCRIPT_DIRECTORY/../functions/services.sh"
 # ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 # ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
 
-# TODO: Cant change if other greeter is enabled:
-# TODO: Failed to enable unit: File /etc/systemd/system/display-manager.service already exists and is a symlink to /usr/lib/systemd/system/sddm.service.
-
 # Constant variables for greeter configuration.
 GREETER_PACKAGE="ly"
+GREETER_SERVICE="/etc/systemd/system/display-manager.service"
 GREETER_CONFIGURATION="/etc/ly/config.ini"
 GREETER_BLANK_PASSWORD="blank_password"
 GREETER_BLANK_PASSWORD_TRUE="blank_password = true"
+
+# Check if another greeter is already enabled.
+if [ -L "$GREETER_SERVICE" ]; then
+    existing_greeter=$(readlink "$GREETER_SERVICE")
+    log_info "Disabling $existing_greeter greeter..."
+    sudo systemctl disable "$existing_greeter"
+fi
 
 # Install display manager.
 install_packages "$GREETER_PACKAGE" "$AUR_PACKAGE_MANAGER" "Installing display manager..."
