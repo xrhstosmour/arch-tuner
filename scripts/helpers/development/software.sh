27b84a44e74c0c63d4e7c75dddd99bfa44cd26fd
Christos <xrhstosmour@gmail.com>
2023-11-17 13:35:31 +0200

    Import missing flags

--- scripts/helpers/development/software.sh
+++ scripts/helpers/development/software.sh
@@ -12,6 +12,7 @@ SOFTWARE_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Import functions.
 source "$SOFTWARE_SCRIPT_DIRECTORY/../functions/packages.sh"
 source "$SOFTWARE_SCRIPT_DIRECTORY/../functions/services.sh"
+source "$SOFTWARE_SCRIPT_DIRECTORY/../../core/flags.sh"
 
 # ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 # ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
--- scripts/utilities/privacy.sh
+++ scripts/utilities/privacy.sh
@@ -12,6 +12,7 @@ PRIVACY_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Import functions.
 source "$PRIVACY_SCRIPT_DIRECTORY/../helpers/functions/filesystem.sh"
 source "$PRIVACY_SCRIPT_DIRECTORY/../helpers/functions/packages.sh"
+source "$PRIVACY_SCRIPT_DIRECTORY/../core/flags.sh"
 
 # ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 # ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
