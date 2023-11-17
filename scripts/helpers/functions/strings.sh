846bfa9bc8c7fedf2ca0b65dc0e8f6c3eed6535a
Christos <xrhstosmour@gmail.com>
2023-11-17 17:59:53 +0200

    Add quotes when changing string flags

--- scripts/helpers/functions/strings.sh
+++ scripts/helpers/functions/strings.sh
@@ -15,5 +15,15 @@ change_flag_value() {
     local flag=$1
     local value=$2
     local script_path=$3
-    sed -i "s/^$flag=.*\$/$flag=$value/" "$script_path"
+
+    # Check if the value is an integer.
+    if [[ $value =~ ^[0-9]+$ ]]; then
+
+        # If it's an integer, don't add quotes.
+        sed -i "s/^$flag=.*\$/$flag=$value/" "$script_path"
+    else
+
+        # If it's not an integer, add quotes.
+        sed -i "s/^$flag=.*\$/$flag=\"$value\"/" "$script_path"
+    fi
 }
