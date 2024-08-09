b364baa832ed56c6586ecf9683e539e35db6111f
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2024-08-09 20:35:35 +0300

    fixup! chore: Refresh mirror list on startup using the `rate-mirrors` package

--- scripts/helpers/essentials/mirrors.sh
+++ scripts/helpers/essentials/mirrors.sh
@@ -35,7 +35,7 @@ if [ ! -f "$RATE_MIRRORS_SERVICE_FILE" ]; then
     # If the rate-mirrors command is not in the root user's crontab, add it.
     if ! sudo crontab -l | grep -q "@reboot $RATE_MIRRORS_COMMAND"; then
         log_info "Adding mirror list auto refresh command to the root user's crontab..."
-        (crontab -l 2>/dev/null; echo "@reboot $RATE_MIRRORS_COMMAND") | sudo crontab -
+        sudo echo "@reboot $RATE_MIRRORS_COMMAND" >> /etc/crontab
     fi
 
     log_info "Creating mirror list auto refresh service..."
@@ -43,7 +43,7 @@ if [ ! -f "$RATE_MIRRORS_SERVICE_FILE" ]; then
     Description=Mirror list auto refresh service on startup
 
     [Service]
-    ExecStart=/bin/bash -c '$RATE_MIRRORS_COMMAND'
+    ExecStart=/bin/bash -c "$RATE_MIRRORS_COMMAND"
     Type=oneshot
     RemainAfterExit=yes
 
@@ -51,7 +51,7 @@ if [ ! -f "$RATE_MIRRORS_SERVICE_FILE" ]; then
     WantedBy=multi-user.target" | sudo tee "$RATE_MIRRORS_SERVICE_FILE" >/dev/null
 
     log_info "Configuring mirror list..."
-    /bin/bash -c '$RATE_MIRRORS_COMMAND'
+    /bin/bash -c "$RATE_MIRRORS_COMMAND"
 fi
 
 # Start and enable mirror list auto update service if it is not already active/enabled.
