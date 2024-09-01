8eedd44b0ab22337853cd99986ddcbd761837220
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2024-09-01 16:42:21 +0300

    feat: Enable cache clearing after package installation

--- scripts/helpers/essentials/pacman.sh
+++ scripts/helpers/essentials/pacman.sh
@@ -17,9 +17,10 @@ source "$PACMAN_SCRIPT_DIRECTORY/../functions/packages.sh"
 
 # Constant variables for configuring the Arch package manager.
 PACMAN_CONFIGURATION="/etc/pacman.conf"
+PACCACHE_HOOK_FILE="/usr/share/libalpm/hooks/paccache.hook"
 
 # Configure Arch package manager.
-if ! grep -q '^Color' "$PACMAN_CONFIGURATION" || ! grep -q '^ParallelDownloads' "$PACMAN_CONFIGURATION"; then
+if ! grep -q '^Color' "$PACMAN_CONFIGURATION" || ! grep -q '^ParallelDownloads' "$PACMAN_CONFIGURATION" || [ ! -f "$PACCACHE_HOOK_FILE" ]; then
     log_info "Configuring $ARCH_PACKAGE_MANAGER package manager..."
 fi
 
@@ -33,4 +34,21 @@ fi
 if ! grep -q '^ParallelDownloads' $PACMAN_CONFIGURATION; then
     log_info "Enabling parallel downloads..."
     sudo sed -i '/^#.*ParallelDownloads/s/^#//' $PACMAN_CONFIGURATION
+fi
+
+# Enable cache clearing after package installation.
+install_packages "pacman-contrib" "$ARCH_PACKAGE_MANAGER" "Installing cache clearing package..."
+if [ ! -f "$PACCACHE_HOOK_FILE" ]; then
+    log_info "Enabling cache clearing after package installation, update, or removal..."
+    echo "[Trigger]
+Operation = Upgrade
+Operation = Install
+Operation = Remove
+Type = Package
+Target = *
+
+[Action]
+Description = Cleaning pacman cache …
+When = PostTransaction
+Exec = /usr/bin/paccache -r" | sudo tee "$PACCACHE_HOOK_FILE" >/dev/null
 fi
