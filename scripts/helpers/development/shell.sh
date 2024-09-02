484fe44ce53b274cb5e1ba4d7652fae620d749ee
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2024-09-03 00:40:41 +0300

    feat: Configure shell for development purposes

---
+++ scripts/helpers/development/shell.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Catch exit signal (CTRL + C), to terminate the whole script.
+trap "exit" INT
+
+# Terminate script on error.
+set -e
+
+# Constant variable of the scripts' working directory to use for relative paths.
+SHELL_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Import functions.
+source "$SHELL_SCRIPT_DIRECTORY/../functions/packages.sh"
+source "$SHELL_SCRIPT_DIRECTORY/../functions/filesystem.sh"
+
+# ? Importing constants.sh is not needed, because it is already sourced in the logs script.
+# ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
+
+# Constant variables for configuring shell.
+FISH_CONFIGURATION="$HOME/.config/fish/config.fish"
+FISH_ABBREVIATIONS="$HOME/.config/fish/conf.d/abbr.fish"
+FISH_ABBREVIATIONS_TO_PASS="$SHELL_SCRIPT_DIRECTORY/../../configurations/development/shell/abbreviations.fish"
+FISH_FUNCTIONS="$HOME/.config/fish/functions"
+FISH_FUNCTIONS_TO_PASS="$SHELL_SCRIPT_DIRECTORY/../../configurations/development/shell/functions"
+
+# Check if FISH_ABBREVIATIONS_TO_PASS is contained in FISH_ABBREVIATIONS and append if not.
+is_abbreviations_file_contained=$(is_file_contained_in_another "$FISH_ABBREVIATIONS" "$FISH_ABBREVIATIONS_TO_PASS")
+if [ "$is_abbreviations_file_contained" = "false" ]; then
+    log_info "Appending development abbreviations to the shell configuration..."
+    cat "$FISH_ABBREVIATIONS_TO_PASS" | sudo tee -a "$FISH_ABBREVIATIONS" > /dev/null
+fi
+
+# Configure shell functions.
+configure_fish_shell_files "$FISH_CONFIGURATION" "$FISH_FUNCTIONS_TO_PASS" "$FISH_FUNCTIONS" "development functions"
+
--- scripts/utilities/development.sh
+++ scripts/utilities/development.sh
@@ -15,6 +15,9 @@ source "$DEVELOPMENT_SCRIPT_DIRECTORY/../helpers/functions/filesystem.sh"
 # ? Importing constants.sh is not needed, because it is already sourced in the logs script.
 # ? Importing logs.sh is not needed, because it is already sourced in the other function scripts.
 
+# Configure shell for development purposes.
+sh $DEVELOPMENT_SCRIPT_DIRECTORY/../helpers/development/shell.sh
+
 # Install and configure software.
 sh $DEVELOPMENT_SCRIPT_DIRECTORY/../helpers/development/software.sh
 
