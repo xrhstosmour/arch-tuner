15db8b14e98e3cde8a4acbbbcefb40dbc26c0cdd
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2026-07-31 14:50:31 +0300

    refactor: restructure for server-only purpose

--- scripts/core/flags.sh
+++ scripts/core/flags.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Flags to mark the completion of each section.
+# Initial setup flag (0: not set, 1: set)
 INITIAL_SETUP=0
-SYSTEM_RESET=1
+
+# System reset flag (0: not set, 1: set)
+SYSTEM_RESET=0
+
+# Phase completion flags
 ESSENTIALS_COMPLETED=1
-INTERFACE_COMPLETED=1
 PRIVACY_COMPLETED=1
-DEVELOPMENT_COMPLETED=1
-DESKTOP_COMPLETED=1
 SECURITY_COMPLETED=1
