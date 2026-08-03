7d6a9e4e44317a53a10ed90ac384cc8d26ec9130
Christos Mourtogiannis <c.mourtogiannis@gmail.com>
2026-08-03 13:52:24 +0300

    Wire new security helpers into `security.sh`

--- scripts/utilities/security.sh
+++ scripts/utilities/security.sh
@@ -50,3 +50,36 @@ sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/ids.sh
 
 # Configure mount points for extra hardening.
 sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/mount.sh
+
+# Create administrative user.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/user.sh
+
+# Configure SSH server hardening.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/ssh.sh
+
+# Harden sudoers configuration.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/sudoers.sh
+
+# Install and configure fail2ban.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/fail2ban.sh
+
+# Apply kernel sysctl hardening.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/sysctl.sh
+
+# Harden systemd services.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/systemd.sh
+
+# Configure systemd journald.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/journald.sh
+
+# Configure AIDE integrity monitoring.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/aide.sh
+
+# Configure audit daemon.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/auditd.sh
+
+# Configure automatic system updates.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/automatic-updates.sh
+
+# Harden Docker engine.
+sh $SECURITY_SCRIPT_DIRECTORY/../helpers/security/docker-engine.sh
