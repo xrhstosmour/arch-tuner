022f6821d062e375b8ebd349cb9c09e5f2bf1818
Christos Mourtogiannis <cmourtogiannis@skroutz.gr>
2026-08-09 16:23:21 +0300

    Add trimmed fastfetch config for headless server

--- scripts/helpers/essentials/terminal.sh
+++ scripts/helpers/essentials/terminal.sh
@@ -11,9 +11,15 @@ TERMINAL_SCRIPT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Import functions.
 source "$TERMINAL_SCRIPT_DIRECTORY/../functions/packages.sh"
+source "$TERMINAL_SCRIPT_DIRECTORY/../functions/filesystem.sh"
 
 # Constant variable for the file path containing the terminal tools to install.
 TERMINAL_TOOLS="$TERMINAL_SCRIPT_DIRECTORY/../../packages/essentials/terminal.txt"
+
+# Constant variables for configuring fastfetch.
+FASTFETCH_CONFIGURATION_DIRECTORY="$HOME/.config/fastfetch"
+FASTFETCH_CONFIGURATION="$HOME/.config/fastfetch/config.jsonc"
+FASTFETCH_CONFIGURATION_TO_PASS="$TERMINAL_SCRIPT_DIRECTORY/../../configurations/essentials/terminal/fastfetch/config.jsonc"
 
 # Check if at least one terminal tool is not installed.
 are_terminal_packages_installed=$(are_packages_installed "$TERMINAL_TOOLS" "$AUR_PACKAGE_MANAGER")
@@ -22,4 +28,12 @@ if [ "$are_terminal_packages_installed" = "false" ]; then
 
     # Install terminal tools.
     install_packages "$TERMINAL_TOOLS" "$AUR_PACKAGE_MANAGER"
+fi
+
+# Configure fastfetch.
+are_fastfetch_configuration_files_the_same=$(compare_files "$FASTFETCH_CONFIGURATION" "$FASTFETCH_CONFIGURATION_TO_PASS")
+if [ "$are_fastfetch_configuration_files_the_same" = "false" ]; then
+    log_info "Configuring fastfetch..."
+    mkdir -p "$FASTFETCH_CONFIGURATION_DIRECTORY"
+    cp -f "$FASTFETCH_CONFIGURATION_TO_PASS" "$FASTFETCH_CONFIGURATION"
 fi
